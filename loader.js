1908a86c21c1b60a54c2faeec69db6c368ca7ef9
icr0z <84248928+icr0z@users.noreply.github.com>
2024-11-16 01:39:17 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -42,12 +42,12 @@ GetTranslationModule().then(() => {
       const command = require(`./commands/${dirs}/${file}`);
       if (command.name && command.description) {
         commandsArray.push(command);
-        const txtEvent = `< -> > [Loaded Command] <${command.name.toLowerCase()}>`;
+        const txtEvent = `< -> > [Loaded Command] <${command.name} [${dirs}]>`;
         parseLog(txtEvent);
         client.commands.set(command.name.toLowerCase(), command);
         delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
       } else {
-        const txtEvent = `< -> > [Failed Command] <${command.name.toLowerCase()}>`;
+        const txtEvent = `< -> > [Failed Command] <${command.name} [${dirs}]>`;
         parseLog(txtEvent);}
     }
   });
