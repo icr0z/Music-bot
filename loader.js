0f9101110936352224dbff5d7d19781e7d8e050a
icr0z <84248928+icr0z@users.noreply.github.com>
2024-11-19 01:27:57 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -42,14 +42,23 @@ GetTranslationModule().then(() => {
       const command = require(`./commands/${dirs}/${file}`);
       if (command.name && command.description) {
         commandsArray.push(command);
-        const txtEvent = `< -> > [Loaded Command] <${command.name} [${dirs}]>`;
+        const txtEvent = `< -> > [Loaded Command] <${command.name.toLowerCase()}>`;
         parseLog(txtEvent);
         client.commands.set(command.name.toLowerCase(), command);
         delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
       } else {
-        const txtEvent = `< -> > [Failed Command] <${command.name} [${dirs}]>`;
+        const txtEvent = `< -> > [Failed Command] <${command.name.toLowerCase()}>`;
         parseLog(txtEvent);}
     }
+  });
+
+  client.on("ready", (client) => {
+    if (client.config.app.global)
+      client.application.commands.set(commandsArray);
+    else
+      client.guilds.cache
+        .get(client.config.app.guild)
+        .commands.set(commandsArray);
   });
 
   async function parseLog(txtEvent) {
